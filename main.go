7c888fae795a65d62f5333b36dc31e252a4e45ec
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-04-22 21:26:19 +0100

    fetch data func and dev flag

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -12,8 +13,8 @@ import (
 )
 
 const (
-	devConfig  = "config.development.json"
-	prodConfig = "config.production.json"
+	DEV_CONFIG  = "config.development.json"
+	PROD_CONFIG = "config.production.json"
 )
 
 type Config struct {
@@ -22,10 +23,12 @@ type Config struct {
 		Database           string `json:"databaseName"`
 		IntegratedSecurity bool   `json:"integratedSecurity"`
 		ApplicationIntent  string `json:"applicationIntent"`
+		ApplicationName    string `json:"applicationName"`
 	} `json:"database"`
 }
 
 type Period struct {
+	ID             int
 	PeriodStart    time.Time
 	PeriodEnd      time.Time
 	Price          float64
@@ -48,11 +51,13 @@ func readConfig(path string) (*Config, error) {
 
 // Connect to the dabatase
 func connectDB(cfg Config) (*sql.DB, error) {
-	connStr := fmt.Sprintf("server=%s;database=%s;integrated security=%t;application intent=%s",
+	connStr := fmt.Sprintf("server=%s;database=%s;integrated security=%t;application intent=%s; application name=%s",
 		cfg.Database.Server,
 		cfg.Database.Database,
 		cfg.Database.IntegratedSecurity,
-		cfg.Database.ApplicationIntent)
+		cfg.Database.ApplicationIntent,
+		cfg.Database.ApplicationName)
+	fmt.Printf("Connection string: %s\n", connStr)
 	db, err := sql.Open("mssql", connStr)
 	if err != nil {
 		return nil, fmt.Errorf("error connecting to the database: %w", err)
@@ -60,20 +65,76 @@ func connectDB(cfg Config) (*sql.DB, error) {
 	return db, nil
 }
 
+func fetchPeriods(db *sql.DB, queryPath string) ([]Period, error) {
+	// read sql query from file
+	query, err := os.ReadFile(queryPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read query from file: %w", err)
+	}
+
+	// execute sql query
+	rows, err := db.Query(string(query))
+	if err != nil {
+		return nil, fmt.Errorf("query execution failed: %w", err)
+	}
+	defer rows.Close() // close rows after processing
+
+	var periods []Period
+	for rows.Next() {
+		var p Period // scan each rows into Period struct
+		// Scan field order must match sql query field order
+		if err := rows.Scan(
+			&p.ID,
+			&p.PeriodStart,
+			&p.PeriodEnd,
+			&p.Price,
+			&p.ProdNum,
+			&p.PeriodPriority); err != nil {
+			return nil, fmt.Errorf("error scanning period: %w", err)
+		}
+		periods = append(periods, p)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("error reading rows: %w", err)
+	}
+
+	return periods, nil
+}
+
 func main() {
+	devFlag := flag.Bool("dev", false, "Set to true to run in development mode")
+	flag.Parse()
+	var envConfig string
+
+	if *devFlag {
+		fmt.Println("Running in development mode")
+		envConfig = DEV_CONFIG
+	} else {
+		fmt.Println("Running in production mode")
+		envConfig = PROD_CONFIG
+	}
+
 	// add env flag
-	config, err := readConfig(devConfig)
+	config, err := readConfig(envConfig)
 	if err != nil {
 		log.Fatal("Config error: ", err)
 	}
 
 	fmt.Println(config)
 
+	// connect to db
 	db, err := connectDB(*config)
 	if err != nil {
 		log.Fatal("Database connection error: ", err)
 	} else {
-		fmt.Printf("Connected successfully to server %s, database name %s.", config.Database.Server, config.Database.Database)
+		fmt.Printf("Connected successfully to server %s, database name %s.\n", config.Database.Server, config.Database.Database)
 	}
 	defer db.Close()
+
+	// fetch data
+
+	// process data
+
+	// output processed data
 }
